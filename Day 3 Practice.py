3ff5f73f2e6eda42b2c8aec71af6d99ebfb892fe
Peter Jiang <peter.j@usask.ca>
2024-10-08 15:23:54 -0600

    Check Number Is Even or Odd

--- Day 3 Practice.py
+++ Day 3 Practice.py
@@ -1,7 +1,16 @@
-print("Welcome to the rollercoaster!")
-height = int(input("What is your height in cm? "))
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is your height in cm? "))
+#
+# if height > 120:
+#     print("You can ride the rollercoaster")
+# else:
+#     print("Sorry you have to grow taller before you can ride")
 
-if height > 120:
-    print("You can ride the rollercoaster")
+print(10 % 3)
+
+number = int(input("What is the number you want to check? "))
+
+if number % 2 == 0:
+    print(f"{number} is a even number!")
 else:
-    print("Sorry you have to grow taller before you can ride")
+    print(f"{number} is a odd number!")
