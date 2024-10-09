82292e1dfeefbe7ca50552851395bd8609638f34
Peter Jiang <peter.j@usask.ca>
2024-10-09 16:51:30 -0600

    Generate A Random Floating Point Number

--- Day 4_Randomisation and Python Lists/Day 4 Practice.py
+++ Day 4_Randomisation and Python Lists/Day 4 Practice.py
@@ -1,7 +1,10 @@
 import my_module
 import random
 
-random_integer = random.randint(1,10)
-print(random_integer)
-print(my_module.my_favourite_number)
+# random_integer = random.randint(1,10)
+# print(random_integer)
+# print(my_module.my_favourite_number)
+
+random_number_0_to_1 = random.random() * 10
+print(random_number_0_to_1)
 
