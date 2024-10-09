1b19d790660daa6acaba0592d0ac59b1103fa283
Peter Jiang <peter.j@usask.ca>
2024-10-09 16:58:27 -0600

    Toss A Coin

--- Day 4_Randomisation and Python Lists/Day 4 Practice.py
+++ Day 4_Randomisation and Python Lists/Day 4 Practice.py
@@ -5,6 +5,16 @@ import random
 # print(random_integer)
 # print(my_module.my_favourite_number)
 
-random_number_0_to_1 = random.random() * 10
-print(random_number_0_to_1)
+# # 1 inclusively to 10 exclusively
+# random_number_0_to_1 = random.random() * 10
+# # print(random_number_0_to_1)
+#
+# # 1 to 10 inclusively
+# random_float = random.uniform(1, 10)
+# print(random_float)
 
+random_heads_or_tails = random.randint(0,1)
+if random_heads_or_tails == 0:
+    print("Head")
+else:
+    print("Tail")
