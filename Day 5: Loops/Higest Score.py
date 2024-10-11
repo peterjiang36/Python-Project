856ad1071aee0c762e9c6f27a6fbb5cf54f577ae
Peter Jiang <peter.j@usask.ca>
2024-10-10 21:38:00 -0600

    Find the Highest Score
    Using max() function and looping

---
+++ Day 5: Loops/Higest Score.py
@@ -0,0 +1,18 @@
+student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86]
+
+total_exam_score = sum(student_scores)
+print(total_exam_score)
+
+score_sum = 0
+for score in student_scores:
+    score_sum += score
+print(score_sum)
+
+print(max(student_scores))
+
+max_score = 0
+
+for score in student_scores:
+    if score > max_score:
+        max_score = score
+print(max_score)
