d97bc54ee6f78743f420e9636295e8cb91e9b651
Peter Jiang <peter.j@usask.ca>
2024-10-14 18:22:21 -0600

    Step 1
    TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
    
    TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
    
    TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.

--- Day 7: Hangman/Day 7 Final Project: Hangman.py
+++ Day 7: Hangman/Day 7 Final Project: Hangman.py
@@ -1,9 +1,18 @@
-#Step 1
+# Step 1
+
+import random
 
 word_list = ["aardvark", "baboon", "camel"]
 
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+chosen_word = random.choice(word_list)
+print(chosen_word)
 
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+guess = input("Guess a letter which could be in the word: ").lower()
+print(guess)
+# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+if guess in chosen_word:
+    print("Right")
+else:
+    print("Wrong")
