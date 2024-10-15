f93a278ddf5df344242c92fc8b9554825dfc4160
Peter Jiang <peter.j@usask.ca>
2024-10-14 21:25:02 -0600

    Step 2: Replacing Blanks with Guesses
    TODO -1: Create a "placeholder" with the same number of blanks as the chosen_word
    
    TODO-2: Create a "display" that puts the buess letter in the right postions and _ in other positions

--- Day 7: Hangman/Day 7 Final Project: Hangman.py
+++ Day 7: Hangman/Day 7 Final Project: Hangman.py
@@ -7,12 +7,29 @@ word_list = ["aardvark", "baboon", "camel"]
 # TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 chosen_word = random.choice(word_list)
 print(chosen_word)
+placeholders = ""
+for count in range(1, len(chosen_word) + 1):
+    placeholders += "_"
+
+for placeholder in placeholders:
+    print(placeholder, end="")
+print("\n")
 
 # TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 guess = input("Guess a letter which could be in the word: ").lower()
 print(guess)
+
 # TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-if guess in chosen_word:
-    print("Right")
-else:
-    print("Wrong")
+
+display = ""
+
+for letter in chosen_word:
+    if letter == guess:
+        display += letter
+    else:
+        display += "_"
+
+print(display)
+
+
+
