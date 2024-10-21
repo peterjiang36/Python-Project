3e94a7372d87819c8e92a9daaee6c3f8f12ab18a
Peter Jiang <peter.j@usask.ca>
2024-10-21 17:44:32 -0600

    Create a function with parameter

---
+++ Day 8: Function Parameters & Caesar Cipher/Day 8 Practice.py
@@ -0,0 +1,16 @@
+# def greet():
+#     print("Hello!")
+#     print("Thanks for coming!")
+#     print("Hope you enjoy your time here!")
+#
+#
+# greet()
+
+
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"How do you do {name}?")
+
+
+greet_with_name("Angela")
+greet_with_name("peter")
