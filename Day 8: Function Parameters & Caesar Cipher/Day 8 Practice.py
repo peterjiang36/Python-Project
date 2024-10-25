8c41e8e2441d62cdd64eca7c83820a5dfcc26fbd
Peter Jiang <peter.j@usask.ca>
2024-10-25 16:34:29 -0600

    Function with two parameters

--- Day 8: Function Parameters & Caesar Cipher/Day 8 Practice.py
+++ Day 8: Function Parameters & Caesar Cipher/Day 8 Practice.py
@@ -7,10 +7,17 @@
 # greet()
 
 
-def greet_with_name(name):
-    print(f"Hello {name}")
-    print(f"How do you do {name}?")
+# def greet_with_name(name):
+#     print(f"Hello {name}")
+#     print(f"How do you do {name}?")
+#
+#
+# greet_with_name("Angela")
+# greet_with_name("peter")
+
+def greet_with(name, location):
+    print(f"Hello {name}!")
+    print(f"What is it like in {location}?")
 
 
-greet_with_name("Angela")
-greet_with_name("peter")
+greet_with("Peter", "Saskatoon")
