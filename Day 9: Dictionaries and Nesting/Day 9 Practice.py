1f61702d90a797955079263a8b3639208aa9d2cd
Peter Jiang <peter.j@usask.ca>
2024-12-17 12:47:03 -0600

    loop the dictionary

--- Day 9: Dictionaries and Nesting/Day 9 Practice.py
+++ Day 9: Dictionaries and Nesting/Day 9 Practice.py
@@ -3,13 +3,19 @@ programming_dictionary = {
     "Function": "A piece of code that you can easily call over and over again"
 }
 
-print(programming_dictionary["Bug"])
+# print(programming_dictionary["Bug"])
 
 programming_dictionary["Loop"] = "The action of doint something over and over again."
-print(programming_dictionary)
+# print(programming_dictionary)
 
 
 # programming_dictionary = {}
 # print(programming_dictionary)
 programming_dictionary["Bug"] = "A moth in your computer"
-print(programming_dictionary)
+# print(programming_dictionary)
+
+# loop through a dictionary
+
+for key in programming_dictionary:
+    print(key)
+    print(programming_dictionary[key])
