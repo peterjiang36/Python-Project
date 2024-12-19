5661a85d481740e9c651adf810c235713ccafad5
Peter Jiang <peter.j@usask.ca>
2024-12-18 19:11:42 -0600

    put the function output as a parameter in another function

--- Day 10: Function with Outputs/Day 10 Practice.py
+++ Day 10: Function with Outputs/Day 10 Practice.py
@@ -1,13 +1,23 @@
-def my_function():
-    result = 3 * 2
-    return result
+# def my_function():
+#     result = 3 * 2
+#     return result
+#
+#
+# def format_name(f_name, l_name):
+#     formated_f_name = f_name.title()
+#     formated_l_name = l_name.title()
+#
+#     return f"{formated_f_name} {formated_l_name}"
+#
+#
+# print(format_name("PeTeR", "JiANg"))
 
 
-def format_name(f_name, l_name):
-    formated_f_name = f_name.title()
-    formated_l_name = l_name.title()
+def function_1(text):
+    return text + text
 
-    print(f"{formated_f_name} {formated_l_name}")
+def function_2(text):
+    return text.title()
 
-
-format_name("PeTeR", "JiANg")
+output = function_2(function_1("hello"))
+print(output)
