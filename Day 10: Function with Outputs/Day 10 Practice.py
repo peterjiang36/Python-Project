08456dc7099e1b7707f1c60fc97c41b645d31762
Peter Jiang <peter.j@usask.ca>
2024-12-18 19:05:09 -0600

    function output (formate name)

--- Day 10: Function with Outputs/Day 10 Practice.py
+++ Day 10: Function with Outputs/Day 10 Practice.py
@@ -2,3 +2,12 @@ def my_function():
     result = 3 * 2
     return result
 
+
+def format_name(f_name, l_name):
+    formated_f_name = f_name.title()
+    formated_l_name = l_name.title()
+
+    print(f"{formated_f_name} {formated_l_name}")
+
+
+format_name("PeTeR", "JiANg")
