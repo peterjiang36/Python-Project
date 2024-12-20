042f39701510cb26f69526bb3157b4f9fa8b4abf
Peter Jiang <peter.j@usask.ca>
2024-12-20 13:38:14 -0600

    Calculator basic logic implementation

--- Day 10: Function with Outputs/Day 10 Final Project: Calculator.py
+++ Day 10: Function with Outputs/Day 10 Final Project: Calculator.py
@@ -1,3 +1,33 @@
 import art
 
 print(art.logo)
+
+first_num = float(input("What's the first number?: "))
+
+operation = input("+\n-\n*\n/\nPick an operation: ")
+# Exception control: in case the user enter invalid operation
+if operation != "+" and "-" and "*" and "/":
+    operation = input("Invalid input! Please choose a operation in '+, -, * and /: ")
+
+second_num = float(input("What's the next number?: "))
+
+if operation == "+":
+    result = first_num + second_num
+elif operation == "-":
+    result = first_num - second_num
+elif operation == "*":
+    result = first_num * second_num
+elif operation == "/":
+    if second_num == 0:
+        second_num = float(input("Invalid input! The denominator cannot be 0! Please re-enter a non 0 number: "))
+        result = first_num / second_num
+    else:
+        result = first_num / second_num
+
+print(f"{first_num} {operation} {second_num} = {result}")
+
+
+
+# while continue_calcualte:
+#     result = None
+#     input("Wh")
