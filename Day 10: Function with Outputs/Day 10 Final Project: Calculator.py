73dd34f3ddab998111c802e3a98e068c2d5b0425
Peter Jiang <peter.j@usask.ca>
2024-12-20 23:57:36 -0600

    using helper function

--- Day 10: Function with Outputs/Day 10 Final Project: Calculator.py
+++ Day 10: Function with Outputs/Day 10 Final Project: Calculator.py
@@ -2,32 +2,63 @@ import art
 
 print(art.logo)
 
+
+def add(n1, n2):
+    return n1 + n2
+
+
+def sub(n1, n2):
+    return n1 - n2
+
+
+def mul(n1, n2):
+    return n1 * n2
+
+
+def div(n1, n2):
+    return n1 / n2
+
+
+operations = {
+    "+": add,
+    "-": sub,
+    "*": mul,
+    "/": div}
+
 first_num = float(input("What's the first number?: "))
 
-operation = input("+\n-\n*\n/\nPick an operation: ")
-# Exception control: in case the user enter invalid operation
-if operation != "+" and "-" and "*" and "/":
-    operation = input("Invalid input! Please choose a operation in '+, -, * and /: ")
+continue_calculate = True
 
-second_num = float(input("What's the next number?: "))
+while continue_calculate:
 
-if operation == "+":
-    result = first_num + second_num
-elif operation == "-":
-    result = first_num - second_num
-elif operation == "*":
-    result = first_num * second_num
-elif operation == "/":
-    if second_num == 0:
-        second_num = float(input("Invalid input! The denominator cannot be 0! Please re-enter a non 0 number: "))
-        result = first_num / second_num
-    else:
-        result = first_num / second_num
+    for symbol in operations:
+        print(symbol)
 
-print(f"{first_num} {operation} {second_num} = {result}")
+    operators = input("Pick an operation: ")
 
+    # Exception control: in case the user enter invalid operation
+    is_operator_in_operations = True
+    while is_operator_in_operations:
+        if operators not in operations:
+            operators = input("Invalid input! Please choose a operation in '+', '-', '*' or '/': ")
+        else:
+            is_operator_in_operations = False
 
+    second_num = float(input("What's the next number?: "))
 
-# while continue_calcualte:
-#     result = None
-#     input("Wh")
+    # Exception: in case the user choose divide operation and the denominator is 0
+    if operators == "/" and second_num == 0:
+        is_zero = True
+        while is_zero:
+            second_num = float(input("Invalid input! The denominator cannot be 0! Please re-enter a non 0 number: "))
+            if second_num != 0:
+                is_zero = False
+
+    result = operations[operators](first_num, second_num)
+
+    print(f"{first_num} {operators} {second_num} = {result}")
+
+    choice = input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation: ")
+
+    if choice == "y":
+        first_num = result
