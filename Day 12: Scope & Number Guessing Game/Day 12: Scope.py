9f960d9984fe41025d12b999373e0e2232ae1c2c
Peter Jiang <peter.j@usask.ca>
2025-01-23 17:37:49 -0600

    check if a number is prime

---
+++ Day 12: Scope & Number Guessing Game/Day 12: Scope.py
@@ -0,0 +1,32 @@
+# enemies = 1
+#
+# def increase_enemies():
+#     print(f"enemies inside function: {enemies}")
+#
+#
+# increase_enemies()
+# print(f"enemies outside function: {enemies}")
+#
+# local scope
+# def drink_potion():
+#     potion_strength = 2
+#     print(potion_strength)
+# drink_potion()
+# print(potion_strength)
+
+# # global scope
+#
+# player_health = 10
+# def drink_potion():
+#     potion_strength = 2
+#     print(player_health)
+# drink_potion()
+
+game_level = 10
+enemies = ["Skeleton", "Zombie", "Alien"]
+def create_enemy():
+    new_enemy = ""
+    if game_level < 5:
+        new_enemy = enemies[0]
+    print(new_enemy)
+create_enemy()
